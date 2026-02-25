a248191fa30bef4c6d75335d54678099a32325dd
Jaque Rufino <79699708+jaquelinerufinoKPMG@users.noreply.github.com>
2026-02-25 11:24:23 -0300

    pos processamento do csv

--- processa_anexo_c.py
+++ processa_anexo_c.py
@@ -6,29 +6,29 @@ from tqdm.auto import tqdm
 from src.anexo_c.process_dashboard import process_dashboard
 load_dotenv()
 
-FINAL_CSV = r"data\input\final.csv"  # <-- vem do pipeline anterior
-TEMPLATE_PATH = r"docs\template_csll.xlsx"
+FINAL_CSV = r"C:\dev\superveniencia_piscofins\data\input\final_20260224.csv"  # <-- vem do pipeline anterior
+TEMPLATE_PATH = r"C:\dev\superveniencia_piscofins\docs\template_csll.xlsx"
 
 TAX_FILTERS = {
     "IRPJ": ["RAIR", "Exclusão", "Adição"],
     "CS": ["RAIR", "Exclusão", "Adição"],
 }
 
-OUTPUT_DIR = r"data\output"
+OUTPUT_DIR = r"C:\dev\superveniencia_piscofins\data\output"
 final_all = pd.read_csv(FINAL_CSV, dtype={"NumContrato": "int64"})
 
 cls = process_dashboard(TAX_FILTERS, TEMPLATE_PATH)
-cls.get_contract_numbers(OUTPUT_DIR)
+cls.get_contract_numbers(OUTPUT_DIR, r"C:\dev\superveniencia_piscofins\numeros_extraidos.txt")
 
 
 contratos_to_process = set()
-if os.path.isfile("processar_kpmg.txt"):
-    with open("processar_kpmg.txt", "r", encoding="utf-8") as f:
+if os.path.isfile(r"C:\dev\superveniencia_piscofins\contratos_para_processar_jaq.txt"):
+    with open(r"C:\dev\superveniencia_piscofins\contratos_para_processar_jaq.txt", "r", encoding="utf-8") as f:
         contratos_to_process = {int(line.strip()) for line in f if line.strip().isdigit()}
 
 nao_encontrados_3 = set()
-if os.path.isfile("numeros_extraidos.txt"):
-    with open("numeros_extraidos.txt", "r", encoding="utf-8") as f:
+if os.path.isfile(r"C:\dev\superveniencia_piscofins\numeros_extraidos.txt"):
+    with open(r"C:\dev\superveniencia_piscofins\numeros_extraidos.txt", "r", encoding="utf-8") as f:
         nao_encontrados_3 = {int(line.strip()) for line in f if line.strip().isdigit()}
 
 # Filtre os contratos removendo aqueles que estão na lista de não encontrados
